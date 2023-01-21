215580b3851dc2a3576314cc8ced6fde1fb5d18b
kiplangatsang <kiplangatsang425@gmail.com>
2023-01-21 09:57:17 +0300

    post.blade

--- resources/views/post/post.blade.php
+++ resources/views/post/post.blade.php
@@ -31,7 +31,7 @@
 
                     </div>
                 </div>
-                <div class="col-md-2 col-xl-2 p-1">
+                {{--  <div class="col-md-2 col-xl-2 p-1">
                     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5005358454303953" crossorigin="anonymous"></script>
                     <!-- StormsAdd -->
                     <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-5005358454303953" data-ad-slot="8840350149" data-ad-format="auto" data-full-width-responsive="true"></ins>
@@ -39,7 +39,7 @@
                         (adsbygoogle = window.adsbygoogle || []).push({});
 
                     </script>
-                </div>
+                </div>  --}}
             </div>
         </div>
     </header>
